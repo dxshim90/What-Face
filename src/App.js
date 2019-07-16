81b36f84a07313a243cc53428d9e0b767989c53e
dxshim90 <dxshim90@gmail.com>
2019-07-16 12:15:16 +0100

    background done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import "./App.css";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* // <FaceRecognition />} } */}
     </div>
